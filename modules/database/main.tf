ada948a987eb29c777e268eb31cb1fdba86ce56c
Manu Goyal <manu@braintrustdata.com>
2025-05-02 15:17:52 -0700

    Add pg_partman to list of loaded extensions.
    Used for the logs2 migration

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -73,7 +73,7 @@ resource "aws_db_parameter_group" "main" {
 
   parameter {
     name         = "shared_preload_libraries"
-    value        = "pg_stat_statements,pg_hint_plan,pg_cron"
+    value        = "pg_stat_statements,pg_hint_plan,pg_cron,pg_partman"
     apply_method = "pending-reboot"
   }
 
