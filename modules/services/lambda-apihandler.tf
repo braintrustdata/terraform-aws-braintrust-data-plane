4f7bf6ae9c82a6efc83419a081e04d29b1342914
Mike Deeks <mike@braintrustdata.com>
2025-05-28 15:40:37 -0700

    duckdb-nodejs-arm64 version 14

--- modules/services/lambda-apihandler.tf
+++ modules/services/lambda-apihandler.tf
@@ -25,7 +25,7 @@ resource "aws_lambda_function" "api_handler" {
 
   layers = [
     # See https://github.com/tobilg/duckdb-nodejs-layer
-    "arn:aws:lambda:${data.aws_region.current.name}:041475135427:layer:duckdb-nodejs-arm64:12"
+    "arn:aws:lambda:${data.aws_region.current.name}:041475135427:layer:duckdb-nodejs-arm64:14"
   ]
 
   ephemeral_storage {
