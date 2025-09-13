19721a114cbc8fe4615b476dff42638a3fa06fd8
Hurshal Patel <hurshal@braintrustdata.com>
2025-09-12 18:09:28 -0700

    pass service token secret key to brainstore

--- modules/services/outputs.tf
+++ modules/services/outputs.tf
@@ -62,3 +62,9 @@ output "quarantine_lambda_security_group_id" {
   description = "The ID of the security group for the quarantine Lambda functions"
   value       = var.use_quarantine_vpc ? aws_security_group.quarantine_lambda[0].id : null
 }
+
+output "function_tools_secret_key" {
+  description = "The function tools secret key for SERVICE_TOKEN_SECRET_KEY"
+  value       = aws_secretsmanager_secret_version.function_tools_secret.secret_string
+  sensitive   = true
+}
