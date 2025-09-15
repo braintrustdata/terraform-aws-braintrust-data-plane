45887ccd5386d61e71c046379ff3449f0a55d451
Jeff McCollum <16550786+jeffmccollum@users.noreply.github.com>
2025-09-15 08:35:26 -0500

    Update variables.tf

--- examples/braintrust-data-plane/variables.tf
+++ examples/braintrust-data-plane/variables.tf
@@ -1,6 +1,7 @@
 variable "brainstore_license_key" {
   description = "The license key for the Brainstore instance. You can get this from the Braintrust UI in Settings > Data Plane."
   type        = string
+  default = "this-is-a-tottally-fine-license-key"
   validation {
     condition     = var.brainstore_license_key != null && var.brainstore_license_key != ""
     error_message = "The brainstore_license_key must be set."
