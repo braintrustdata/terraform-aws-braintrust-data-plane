233f46abc2b9841fb9e30089c24dccbfde6d547e
Mike Deeks <mike@braintrustdata.com>
2025-10-03 15:29:14 -0700

    Add comment to example about how to pass in custom_tags

--- examples/braintrust-data-plane/provider.tf
+++ examples/braintrust-data-plane/provider.tf
@@ -6,4 +6,11 @@ provider "aws" {
   # Optional, but recommended. Only allow running in a specific AWS account.
   # This is helpful for preventing accidental changes in the wrong account.
   allowed_account_ids = ["<your AWS account ID>"]
+
+  # Optionally, you can add default tags to all resources created by this module.
+  # default_tags {
+  #   tags = {
+  #     YourCustomTag = "<your-custom-value>"
+  #   }
+  # }
 }
