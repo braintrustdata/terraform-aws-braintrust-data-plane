3a7fc4c7c09568e4901bf1a63002de31f21382a7
Mike Deeks <mike@braintrustdata.com>
2025-10-08 16:20:26 -0700

    Move brainstore iam/sgs to common module

--- modules/brainstore/outputs.tf
+++ modules/brainstore/outputs.tf
@@ -17,8 +17,3 @@ output "brainstore_elb_security_group_id" {
   description = "The ID of the security group for the Brainstore ELB"
   value       = aws_security_group.brainstore_elb.id
 }
-
-output "brainstore_instance_security_group_id" {
-  description = "The ID of the security group for the Brainstore instances"
-  value       = aws_security_group.brainstore_instance.id
-}
--- modules/brainstore/security-groups.tf
+++ modules/brainstore/security-groups.tf
@@ -24,20 +24,14 @@ resource "aws_vpc_security_group_egress_rule" "brainstore_elb_allow_egress_all" 
   security_group_id = aws_security_group.brainstore_elb.id
 }
 
-resource "aws_security_group" "brainstore_instance" {
-  name   = "${var.deployment_name}-brainstore-instance"
-  vpc_id = var.vpc_id
-  tags   = merge({ "Name" = "${var.deployment_name}-brainstore-instance" }, local.common_tags)
-}
-
+# Security group rules for the brainstore instance security group (created by services-common)
 resource "aws_vpc_security_group_ingress_rule" "brainstore_instance_allow_ingress_from_nlb" {
-
   from_port                    = var.port
   to_port                      = var.port
   ip_protocol                  = "tcp"
   referenced_security_group_id = aws_security_group.brainstore_elb.id
   description                  = "Allow inbound to Brainstore instances from NLB."
-  security_group_id            = aws_security_group.brainstore_instance.id
+  security_group_id            = var.brainstore_instance_security_group_id
 }
 
 resource "aws_vpc_security_group_ingress_rule" "brainstore_instance_allow_ingress_from_authorized_security_groups_ssh" {
@@ -48,16 +42,6 @@ resource "aws_vpc_security_group_ingress_rule" "brainstore_instance_allow_ingres
   ip_protocol                  = "tcp"
   referenced_security_group_id = each.value
   description                  = "Allow inbound SSH to Brainstore instances from ${each.key}."
-
-  security_group_id = aws_security_group.brainstore_instance.id
+  security_group_id            = var.brainstore_instance_security_group_id
 }
 
-resource "aws_vpc_security_group_egress_rule" "brainstore_instance_allow_egress_all" {
-
-  from_port         = -1
-  to_port           = -1
-  ip_protocol       = "-1"
-  cidr_ipv4         = "0.0.0.0/0"
-  description       = "Allow all outbound traffic from Brainstore instances."
-  security_group_id = aws_security_group.brainstore_instance.id
-}
