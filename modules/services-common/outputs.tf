f9e5282af4f5c0733004260e3c1723da9f4a5705
Mike Deeks <mike@braintrustdata.com>
2025-10-09 16:54:24 -0700

    Move function secret to services-common

--- modules/services-common/outputs.tf
+++ modules/services-common/outputs.tf
@@ -27,3 +27,9 @@ output "api_security_group_id" {
   description = "The ID of the security group for the API handler"
   value       = aws_security_group.api.id
 }
+
+output "function_tools_secret_key" {
+  description = "The function tools encryption key. This is used by brainstore as the SERVICE_TOKEN_SECRET_KEY."
+  value       = aws_secretsmanager_secret_version.function_tools_secret.secret_string
+  sensitive   = true
+}
