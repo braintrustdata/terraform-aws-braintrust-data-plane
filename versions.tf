fb8b4f1e5caca8041af2207b1e0ed7ba1d0b54d0
Mike Deeks <mike@braintrustdata.com>
2025-10-24 12:50:09 -0700

    Require terraform 1.12 for validation bug fixes (#142)
    > [!WARNING]
    > This PR will require customers to use Terraform v1.12.0 or later. For
    some teams this might require extra work.
    
    Terraform <1.12 has a bug in how it handles variable validation logic.
    Specifically validations couldn't be run against values that are null
    even if there was a null check. This was fixed in terraform 1.12.0.
    
    These validation checks on our side were added because customers (and
    us!) would regularly run into issues with selecting invalid IOPS and
    Throughput values for RDS databases. The rules there are surprisingly
    specific and hard to get right (details here
    https://github.com/braintrustdata/terraform-aws-braintrust-data-plane/pull/139).
    
    Example:
    ```
    Planning failed. Terraform encountered an error while generating this plan.
    
    ╷
    │ Error: Operation failed
    │
    │   on .terraform/modules/braintrust-data-plane/modules/database/variables.tf line 51, in variable "postgres_storage_throughput":
    │   51:       (var.postgres_storage_size >= 400 && var.postgres_storage_throughput != null && var.postgres_storage_throughput >= 500)
    │     ├────────────────
    │     │ var.postgres_storage_throughput is null
    │
    │ Error during operation: argument must not be null.
    ╵
    ```

--- versions.tf
+++ versions.tf
@@ -1,7 +1,8 @@
 terraform {
+  # v1.12 is needed for fixes to variable validations
   # v1.9 is needed for variable validation features
   # v1.2 is needed for precondition checks
-  required_version = ">= 1.9.0"
+  required_version = ">= 1.12.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
