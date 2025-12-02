4335108177b67cfd7470e15ac53df23cbc0ca3cf
Mike Deeks <mike@braintrustdata.com>
2025-12-01 21:31:14 -0800

    Explicitly fill out the source_arn for lambda permissions (#162)
    There appears to be some strange bug in the AWS provider that leaves the
    AccountId part of the API Gateway execution arn empty. It doesn't happen
    to 99% of our users but consistently hits at least two of them now.
    
    Instead of relying on the `.execution_arn` attribute of the API gateway
    the ARN is now manually constructed using the current account ID and
    region configured in the provider. This should be backwards compatible
    for existing users and our test stacks show zero diff in the terraform
    plan.

--- modules/ingress/api-gateway.tf
+++ modules/ingress/api-gateway.tf
@@ -46,5 +46,5 @@ resource "aws_lambda_permission" "api_gateway" {
   action        = "lambda:InvokeFunction"
   function_name = split(":", var.api_handler_function_arn)[6]
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
+  source_arn    = "arn:aws:execute-api:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.api.id}/*/*"
 }
