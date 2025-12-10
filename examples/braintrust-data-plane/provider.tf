0bc2e4e14b12230d6555dc174a8db7de164b2ac6
Mike Deeks <mike@braintrustdata.com>
2025-12-10 14:15:38 -0800

    Comment out some example code (#170)
    Just to ensure it doesn't get used automatically by anyone.

--- examples/braintrust-data-plane/provider.tf
+++ examples/braintrust-data-plane/provider.tf
@@ -1,11 +1,13 @@
-provider "aws" {
-  region = "<your AWS region>"
+# provider "aws" {
+  # region = "<your AWS region>"
+  
   # Optional, but recommended. Use a specific AWS credential profile for creating the Braintrust
   # resources. This helps prevent accidental changes in the wrong account.
-  profile = "<your AWS credential profile>"
+  # profile = "<your AWS credential profile>"
+  
   # Optional, but recommended. Only allow running in a specific AWS account.
   # This is helpful for preventing accidental changes in the wrong account.
-  allowed_account_ids = ["<your AWS account ID>"]
+  # allowed_account_ids = ["<your AWS account ID>"]
 
   # Optionally, you can add default tags to all resources created by this module.
   # default_tags {
@@ -13,4 +15,4 @@ provider "aws" {
   #     YourCustomTag = "<your-custom-value>"
   #   }
   # }
-}
+# }
