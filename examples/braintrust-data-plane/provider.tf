8a410d9410087a888e297b25f9f58499527e22f0
Mike Deeks <mike@braintrustdata.com>
2025-12-10 14:11:59 -0800

    Comment out AWS provider settings
    Comment out AWS provider configuration in provider.tf

--- examples/braintrust-data-plane/provider.tf
+++ examples/braintrust-data-plane/provider.tf
@@ -1,8 +1,10 @@
-provider "aws" {
-  region = "<your AWS region>"
+# provider "aws" {
+  # region = "<your AWS region>"
+  
   # Optional, but recommended. Use a specific AWS credential profile for creating the Braintrust
   # resources. This helps prevent accidental changes in the wrong account.
-  profile = "<your AWS credential profile>"
+  # profile = "<your AWS credential profile>"
+  
   # Optional, but recommended. Only allow running in a specific AWS account.
   # This is helpful for preventing accidental changes in the wrong account.
   allowed_account_ids = ["<your AWS account ID>"]
@@ -13,4 +15,4 @@ provider "aws" {
   #     YourCustomTag = "<your-custom-value>"
   #   }
   # }
-}
+# }
