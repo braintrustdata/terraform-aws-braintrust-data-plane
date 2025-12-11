a6af9d3808ef28ff4c6061939972f8b547708866
Mike Deeks <mike@braintrustdata.com>
2025-12-11 15:58:14 -0800

    Change .name to .region

--- modules/services-common/iam-brainstore.tf
+++ modules/services-common/iam-brainstore.tf
@@ -172,7 +172,7 @@ resource "aws_iam_role_policy" "brainstore_ssm_parameter_access" {
       {
         Effect   = "Allow"
         Action   = "ssm:GetParameter"
-        Resource = "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/braintrust/${var.deployment_name}/ai-proxy-url"
+        Resource = "arn:aws:ssm:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:parameter/braintrust/${var.deployment_name}/ai-proxy-url"
       }
     ]
   })
