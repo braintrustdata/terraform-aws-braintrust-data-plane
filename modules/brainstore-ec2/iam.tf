32b4bec9e0d04e92c6dcf73feb356db4ce838f55
Kevin Green <kevin.green@braintrust.dev>
2026-02-10 16:58:26 -0500

    Add name suffix back ot instance profile

--- modules/brainstore-ec2/iam.tf
+++ modules/brainstore-ec2/iam.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_instance_profile" "brainstore" {
-  name = "${var.deployment_name}-brainstore-instance-profile"
+  name = "${var.deployment_name}-brainstore${local.name_suffix}-instance-profile"
   role = var.brainstore_iam_role_name
 
   tags = local.common_tags
