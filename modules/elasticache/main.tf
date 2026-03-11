bbea39738a89b29d49ba20753c65a461761ef4a7
Mike Deeks <mike@braintrustdata.com>
2026-03-11 14:29:20 -0700

    Egress to 0.0.0.0/0 is not needed for RDS or Elasticache (#212)
    Restrict egress on Elasticache and RDS since they do not need to reach
    out to the internet or even the VPC

--- modules/elasticache/main.tf
+++ modules/elasticache/main.tf
@@ -44,13 +44,3 @@ resource "aws_vpc_security_group_ingress_rule" "elasticache_allow_ingress_from_a
   tags              = local.common_tags
 }
 
-resource "aws_vpc_security_group_egress_rule" "elasticache_allow_egress_all" {
-
-  from_port         = -1
-  to_port           = -1
-  ip_protocol       = "-1"
-  cidr_ipv4         = "0.0.0.0/0"
-  description       = "Allow all outbound traffic from Elasticache instances."
-  security_group_id = aws_security_group.elasticache.id
-  tags              = local.common_tags
-}
