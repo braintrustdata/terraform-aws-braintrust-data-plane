8dd2b7047ba22dc275e919100c7bed6aecb1bcaa
Jeff McCollum <jeff@braintrustdata.com>
2026-05-14 17:05:52 -0500

    add optional abac

--- modules/storage/s3-brainstore.tf
+++ modules/storage/s3-brainstore.tf
@@ -11,6 +11,16 @@ resource "aws_s3_bucket" "brainstore" {
   tags = local.common_tags
 }
 
+resource "aws_s3_bucket_abac" "brainstore" {
+  count = var.enable_s3_bucket_abac ? 1 : 0
+
+  bucket = aws_s3_bucket.brainstore.id
+
+  abac_status {
+    status = "Enabled"
+  }
+}
+
 resource "aws_s3_bucket_server_side_encryption_configuration" "brainstore" {
   bucket = aws_s3_bucket.brainstore.id
 
