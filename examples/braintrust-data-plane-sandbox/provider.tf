74a8b3bd65dce07854cb9c4d338898aeb630050b
Eugene Vignanker <eugene.vignanker@braintrustdata.com>
2026-07-15 11:28:48 -0700

    My changes to setup sandbox

--- examples/braintrust-data-plane-sandbox/provider.tf
+++ examples/braintrust-data-plane-sandbox/provider.tf
@@ -1,13 +1,13 @@
-# provider "aws" {
-#   region = "<your AWS region>"
+provider "aws" {
+   region = "us-east-2"
 
 #   # Optional, but recommended. Use a specific AWS credential profile for creating the Braintrust
 #   # resources. This helps prevent accidental changes in the wrong account.
-#   profile = "<your AWS credential profile>"
+   profile = "default"
 
 #   # Optional, but recommended. Only allow running in a specific AWS account.
 #   # This is helpful for preventing accidental changes in the wrong account.
-#   allowed_account_ids = ["<your AWS account ID>"]
+   allowed_account_ids = ["982534393296"]
 
 #   # Optionally, you can add default tags to all resources created by this module.
 #   default_tags {
@@ -15,4 +15,4 @@
 #       YourCustomTag = "<your-custom-value>"
 #     }
 #   }
-# }
+ }
