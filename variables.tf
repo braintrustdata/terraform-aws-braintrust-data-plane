aa401453f276fef6fcaf091c204750cd6dbdb684
Johnny Carlin <johnny@braintrust.dev>
2026-07-31 12:31:35 -0500

    Use `SET` as the default Redis auth token update strategy
    On new deployments, `ROTATE` has the same behavior as `SET` as there is no original token to rotate. This change removes the need to shift strategy from `ROTATE` to `SET` on new Redis replication group deployments.

--- variables.tf
+++ variables.tf
@@ -360,8 +360,8 @@ variable "use_redis_replication_group" {
 
 variable "redis_rg_auth_token_update_strategy" {
   type        = string
-  description = "Update strategy for the Redis auth token. Use ROTATE for initial deployment, then SET to require authentication."
-  default     = "ROTATE"
+  description = "Update strategy for the Redis auth token."
+  default     = SET"
   validation {
     condition     = contains(["ROTATE", "SET"], var.redis_rg_auth_token_update_strategy)
     error_message = "redis_rg_auth_token_update_strategy must be one of: ROTATE, SET"
