598d205970a41ac50f3262f8f994dab12568d310
Kush Mansingh <12158241+kushmansingh@users.noreply.github.com>
2026-08-19 09:09:48 -0400

    chore: add outputs for cloudwatch log groups

--- modules/gateway-ecs/outputs.tf
+++ modules/gateway-ecs/outputs.tf
@@ -7,3 +7,10 @@ output "task_security_group_id" {
   description = "Security group ID attached to gateway ECS tasks"
   value       = aws_security_group.task.id
 }
+
+output "cloudwatch_log_groups" {
+  description = "Names of the cloudwatch log groups created for the ECS gateway."
+  value = {
+    service = aws_cloudwatch_log_group.service.name
+  }
+}
